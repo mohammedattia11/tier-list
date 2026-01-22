c744dd95585176108e59b8542643959f48e30ffc
mohamed atia <mohamedd.attia11@gmail.com>
2026-01-22 04:51:35 +0200

    feat:adding react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
